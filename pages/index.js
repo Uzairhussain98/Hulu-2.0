df319099844d719624d85d7f0b1149bd9fa01ec8
Uzair Hussain <uzairhussain98@hotmail.com>
2021-06-12 02:30:04 +0500

    added thumbnail

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
 import Nav from '../components/Nav'
+import Results from '../components/Results'
+import Requests from '../utils/Requests'
 
 
-export default function Home() {
+export default function Home({results}) {
   return (
     <div >
       <Head>
@@ -15,6 +17,27 @@ export default function Home() {
 
     <Header/>
     <Nav/>
+    <Results results={results}/>
     </div>
-  )
+  );
 }
+
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${Requests[genre]?.url || Requests.fetchTrending.url}`).then(res => res.json());
+
+  return {
+    props: {
+      results: request.results,
+    }
+  
+
+
+
+
+
+
+
+  }}
